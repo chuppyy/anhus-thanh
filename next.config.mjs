8dc13c75cf4453b897e72563211387d2857227ea
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2026-01-10 20:37:41 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["apisporttest.vbonews.com","apisporttest.vbonews.com"],
+    domains: ["apinewss.vbonews.com","apinewss.vbonews.com"],
   },
 };
 
