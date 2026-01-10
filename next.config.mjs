54b6610449c3024aabe5c8bd0f3942d95a3a028c
chuppyy <chuppyychuppyy@gmail.com>
2026-01-10 20:42:35 +0700

    kk

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["apinewss.vbonews.com","apinewss.vbonews.com"],
+    domains: ["apisporttest.vbonews.com","apisporttest.vbonews.com"],
   },
 };
 
