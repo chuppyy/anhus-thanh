7ff8cc80b678925a6662bbdabc57bf1c2ab967f3
chuppyy <chuppyychuppyy@gmail.com>
2026-01-14 20:29:04 +0700

    kk

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["https://apisportbackup.vbonews.com", "https://apisportbackup.vbonews.com"],
-  },
-};
-
-export default nextConfig;
